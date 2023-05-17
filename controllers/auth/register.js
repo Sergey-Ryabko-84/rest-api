aa7af978c08e5e4ae136448a549902d9ffc02dec
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-17 23:07:59 +0300

    adds ctrl register

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,3 +1,20 @@
-const register = async (req, res) => {};
+const bcrypt = require("bcrypt");
+
+const { User } = require("../../models/user");
+const { HttpError } = require("../../utils");
+
+const register = async (req, res) => {
+    console.log("register");
+  const { email, password } = req.body;
+  existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw HttpError(409, "Email already in use");
+  }
+
+  const hashPass = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPass });
+
+  res.status(201).json({ user: { name: newUser.name, email: newUser.email } });
+};
 
 module.exports = register;
