67921a405e309f539b1ec49a427449082a4e41a7
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-19 23:13:19 +0300

    adds ctrl logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,3 +1,11 @@
-const logout = async (req, res) => {};
+const { User } = require("../../models/user");
+
+const logout = async (req, res) => {
+  const [, token] = req.headers.authorization.split(" ");
+  const newTokens = req.user.token.filter((item) => item !== token);
+
+  await User.findByIdAndUpdate(req.user._id, { token: newTokens });
+  res.json({ massage: "Logout success" });
+};
 
 module.exports = logout;
