548e9ac7ee1e454382a1eeab789c60efc6308536
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-20 23:14:11 +0300

    adds google auth

--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -1,3 +1,36 @@
-const googleRedirect = async (req, res) => {};
+const { default: axios } = require("axios");
+const queryString = require("query-string");
+
+const signInWithAGoogleAccount = require("./signInWithAGoogleAccount");
+
+const googleRedirect = async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+
+  const code = urlParams.code;
+
+  const tokenData = await axios({
+    url: `https://oauth2.googleapis.com/token`,
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: { Authorization: `Bearer ${tokenData.data.access_token}` },
+  });
+
+  const token = await signInWithAGoogleAccount(userData.data);
+
+  return res.redirect(`${process.env.FRONTEND_URL}?token=${token}`);
+};
 
 module.exports = googleRedirect;
---
+++ controllers/auth/signInWithAGoogleAccount.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const generator = require("generate-password");
+const { User } = require("../../models/user");
+
+const signInWithAGoogleAccount = async (data) => {
+  const user = await User.findOne({ email: data.email });
+  if (user) {
+    const payload = { id: user._id };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    user.token.push(token);
+    await User.findByIdAndUpdate(user._id, { token: user.token });
+
+    return token;
+  }
+
+  // signup new user
+  const password = generator.generate({
+    length: 10,
+    numbers: true,
+    symbols: true,
+  });
+  const hashPass = await bcrypt.hash(password, 10);
+
+  await User.create({
+    name: data.name,
+    password: hashPass,
+    email: data.email,
+  });
+
+  // send password to email
+
+  const newUser = await User.findOne({ email: data.email });
+  const payload = { id: newUser._id };
+  const token = jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: "7d",
+  });
+  newUser.token.push(token);
+
+  await User.findByIdAndUpdate(newUser._id, { token: newUser.token });
+
+  return token;
+};
+
+module.exports = signInWithAGoogleAccount;
