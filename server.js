da0f64844cee4df60b34d629ce9ad1a9d4d321ea
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-24 22:02:41 +0300

    adds users router

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () => console.log("Database connection successful"));
+    app.listen(PORT, () =>
+      console.log(
+        `Started listening on port ${PORT}. Database connection successful`
+      )
+    );
   })
   .catch((err) => {
     console.log(err.massage);
