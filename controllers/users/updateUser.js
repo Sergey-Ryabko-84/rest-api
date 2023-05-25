e10fe09dea0748fd596ebb08dc4c58004d921c9d
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-25 23:30:25 +0300

    Update updateUser.js

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,6 +1,11 @@
 const { User } = require("../../models/user");
+const { HttpError } = require("../../utils");
 
 const updateUser = async (req, res) => {
+  if (!req.file && Object.keys(req.body).length === 0) {
+    throw HttpError(400, "missing fields");
+    }
+    
   let { avatarURL } = req.user;
   if (req.file) {
     avatarURL = req.file.path;
