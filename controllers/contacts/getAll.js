73a0d1081463ac64046b8a5296af3ab1bdc21e0d
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-05-28 16:14:03 +0300

    adds ctrl contacts getAll

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,3 +1,20 @@
-const getAll = async (req, res) => {};
+const { Contact } = require("../../models/contact");
+
+const getAll = async (req, res) => {
+  const { page = 1, limit, name, email, phone, status, favorite } = req.query;
+
+  const queryParams = { owner: req.user._id };
+  if (name) queryParams.name = name;
+  if (email) queryParams.email = email;
+  if (phone) queryParams.phone = phone;
+  if (status) queryParams.status = status;
+  if (favorite) queryParams.favorite = favorite;
+
+  const paginationParams = { skip: (page - 1) * limit, limit };
+
+  const contactsList = await Contact.find(queryParams, null, paginationParams);
+
+  res.json(contactsList);
+};
 
 module.exports = getAll;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,6 +5,8 @@ const { schemas } = require("../../models/contact");
 
 const router = Router();
 
+router.get("/", authenticate, ctrl.getAll);
+
 router.get("/:id", authenticate, isValidId, ctrl.getById);
 
 router.post("/", authenticate, validateBody(schemas.addSchena), ctrl.add);
