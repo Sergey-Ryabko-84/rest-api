177e639717f0f7b44dc4113d94ad1c9852b3b340
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-06-04 16:09:27 +0300

    Update getCurrent.js

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,6 +1,11 @@
 const getCurrent = async (req, res) => {
-  const { _id, name, email, avatarURL } = req.user;
-  res.json({ id: _id, name, email, avatarURL });
+  const { _id, name, email, avatarURL, accessToken, refreshToken } = req.user;
+  console.log(req.user);
+  res.json({
+    accessToken,
+    refreshToken,
+    user: { id: _id, name, email, avatarURL },
+  });
 };
 
 module.exports = getCurrent;
